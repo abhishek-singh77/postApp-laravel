b778e07727eff17982ae6155e3f138cf3bb3deca
abhi <assomvanshi7@gmail.com>
2022-04-09 23:53:14 +0530

    created usefull controllers

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,10 +7,36 @@
     <title>PostApp</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body>
+<body class="bg-gray-400">
+    <nav class="p-6 bg-white mb-6 flex justify-between">
+        <ul class="flex items-center">
+            <li>
+                <a href="" class="p-3">Home</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Dashboard</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Posts</a>
+            </li>
+        </ul>
+        <ul class="flex items-center">
+            <li>
+                <a href="" class="p-3">YourName</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Login</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Logout</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Register</a>
+            </li>
+        </ul>
+    </nav>
+    <h1 class="text-center bg-red-300 text-2xl font-bold text-gray-800">PostApp New</h1>
     @yield('content')
-    <h1 class="text-3xl font-bold underline">
-        Hello world!
-    </h1>
+    
 </body>
 </html>
